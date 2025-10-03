820d41a386dd00ffb727e642aadb34c37858d7fb
mokshupadhyay1 <drumsharma9837@gmail.com>
2025-10-03 22:43:47 +0530

    Create combination sum II.java

---
+++ home assignment 6/combination sum II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(candidates, target, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    static void helper(int[] candidates, int target, int start, List<Integer> path, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
+            if (candidates[i] > target) break;
+            path.add(candidates[i]);
+            helper(candidates, target - candidates[i], i + 1, path, res);
+            path.remove(path.size() - 1); 
+        }
+    }
+}
